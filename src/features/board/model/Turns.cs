53e25121160dffa524037f52809b4c59ade19612
andrei g <andrei_g_git@protonmail.com>
2026-06-22 11:59:35 +0300

    fix iterating turn queue stuff after modifying it

--- src/features/board/model/Turns.cs
+++ src/features/board/model/Turns.cs
@@ -82,13 +82,19 @@ public partial class Turns : Node, Sequential, Initializable, /* AccessableBoard
 			}
 		}
 
+		var eliminatedActors = new List<Control>();
 		foreach(var actor in _actors){
 			var cell = Tiles.GetCellFor(actor);
-			if(cell.X < 0 || cell.Y < 0){//<<<<<<<<<<<<<<<<<<<
-				_actors.Remove(actor); //this will give a collection modified error, obv i can't change the list while it's being iterated
-				_turnQueue = new Queue<Control>(_turnQueue.Where(_actor => _actor != actor));
+			if(cell.X < 0 || cell.Y < 0){
+				//_actors.Remove(actor); //this will give a collection modified error, obv i can't change the list while it's being iterated
+				eliminatedActors.Add(actor);
+				//_turnQueue = new Queue<Control>(_turnQueue.Where(_actor => _actor != actor));
 			}
-	
+		}
+
+		foreach(var actor in eliminatedActors){
+			_actors.Remove(actor);
+			_turnQueue = new Queue<Control>(_turnQueue.Where(_actor => _actor != actor));
 		}
 	}
 
